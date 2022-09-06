ea71ba34d94bea54b25329112582e531d2ab61ab
Adrian Radores <redperses143fps@gmail.com>
2022-09-06 17:08:20 +0800

    initial commit
    installed laravel 9 and backpack

---
+++ README.md
@@ -0,0 +1,8 @@
+## LIFE OF POKER 
+
+laravel 9
+backpack admin
+testing framework: PEST
+
+
+
---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+     use CrudTrait; // <----- this
+    use HasRoles; // <------ and this
+    use HasApiTokens, HasFactory, Notifiable;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
