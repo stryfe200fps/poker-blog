05432ec25224f386c3b3598dda23def071c4fd25
Raven Barrogo <barrogoraven@gmail.com>
2022-10-14 17:44:48 +0800

    FIX: image size

--- resources/js/Components/Frontend/Report/ReportList.vue
+++ resources/js/Components/Frontend/Report/ReportList.vue
@@ -196,6 +196,8 @@
                                 v-for="(image, index) in event.gallery"
                                 :key="index"
                                 :href="image.main"
+                                :data-pswp-width="900"
+                                :data-pswp-height="640"
                                 target="_blank"
                                 rel="noreferrer"
                             >
