5d61633726deb22e77b4bf23de513077fd06bd0b
Raven Barrogo <barrogoraven@gmail.com>
2022-10-20 15:23:22 +0800

    FIXED: Article Desc

---
+++ database/migrations/2022_10_20_103706_add_description_in_articles.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->text('description');
+        });
+
+        DB::statement("ALTER TABLE `articles` MODIFY COLUMN `created_at` DATE AFTER `description`");
+        DB::statement("ALTER TABLE `articles` MODIFY COLUMN `updated_at` DATE AFTER `created_at`");
+        DB::statement("ALTER TABLE `articles` MODIFY COLUMN `content` TEXT AFTER `title`");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
