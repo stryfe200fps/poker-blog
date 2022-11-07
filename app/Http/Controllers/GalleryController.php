2c0c72fbbe7a0cf3ecb767e94aeb466803c49b67
Adrian Radores <redperses143fps@gmail.com>
2022-11-07 15:40:05 +0800

    FIX image not showing

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Day;
+use Illuminate\Support\Facades\File;
 
 class GalleryController extends Controller
 {
@@ -19,10 +20,14 @@ class GalleryController extends Controller
         }
 
         foreach ($day->event_reports as $report) {
-            $imgResource[] = [
-              'thumbnail' => $report->getFirstMediaUrl('event-report', 'main-thumb2'),
-              'main' => $report->getFirstMediaUrl('event-report', 'main-image')
-            ];
+            if ($report->has('media')) { 
+                if (File::exists($report->getFirstMediaUrl('event-report', 'main-thumb2'))?true:false) { 
+                $imgResource[] = [
+                'thumbnail' => $report->getFirstMediaUrl('event-report', 'main-thumb2'),
+                'main' => $report->getFirstMediaUrl('event-report', 'main-image')
+                ];
+            }
+            }
         }
 
         // dd($day->event_reports);
