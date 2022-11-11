c1cf161bcfb9ea227b1d14a3bffd4fbfbc0a9242
Adrian <mobistyle35@gmail.com>
2022-11-11 05:16:50 +0000

    FIXED: error in sharing on backend

--- app/Models/EventReport.php
+++ app/Models/EventReport.php
@@ -103,16 +103,12 @@ class EventReport extends Model implements HasMedia
 
     public function shareFacebook()
     {
-        $event = Event::find($this->event->id);
-
-        return '<a class="btn btn-sm btn-link"  href="https://www.facebook.com/sharer/sharer.php?u='.config('app.url').'/tours/tour/series/event/update-'.$event->id.'/report/'.$this->slug.'" data-toggle="tooltip" target="_blank" title="Share to facebook"><i class="la la-facebook"></i>    </a>';
+        return '<a class="btn btn-sm btn-link"  href="https://www.facebook.com/sharer/sharer.php?u='.config('app.url').'/tours/tour/series/event/update-'.$this->id.'" data-toggle="tooltip" target="_blank" title="Share to facebook"><i class="la la-facebook"></i>    </a>';
     }
 
     public function shareTwitter()
     {
-        $event = Event::find($this->event->id);
-
-        return '<a class="btn btn-sm btn-link"  href="https://twitter.com/intent/tweet?text='.config('app.url').'/tours/tour/series/event/update-'.$event->id.'/report/'.$this->slug.'" data-toggle="tooltip" target="_blank" title="Share to facebook"><i class="la la-twitter"></i>    </a>';
+        return '<a class="btn btn-sm btn-link"  href="https://twitter.com/intent/tweet?text='.config('app.url').'/tours/tour/series/event/update-'.$this->id.'" data-toggle="tooltip" target="_blank" title="Share to facebook"><i class="la la-twitter"></i>    </a>';
     }
 
     protected $casts = [
