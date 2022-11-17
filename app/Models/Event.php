4034ee075bd93f392e438c2ac78bfcf59524fd50
Adrian Radores <redperses143fps@gmail.com>
2022-11-17 16:05:51 +0800

    CHANGE: schedule with reports property ordering

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -128,8 +128,8 @@ class Event extends Model implements HasMedia
 
     public function getScheduleWithReports()
     {
-        return $this->days()->orderBy('lft')->orderByDesc('lft')->withCount('event_reports')
-            ->having('event_reports_count', '>', 0 )->pluck('name', 'id');
+        return $this->days()->orderBy('lft')->withCount('event_reports')
+            ->having('event_reports_count', '>', 0 )->get()->map->only('id', 'name');
     }
 
     public function getLastSchedule() 
