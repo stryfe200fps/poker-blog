ed7419e019400162cce01994a487a439593a1dab
Adrian Radores <redperses143fps@gmail.com>
2022-11-25 11:32:29 +0800

    RESET MAIN

--- resources/views/setting_button.blade.php
+++
@@ -1,2 +0,0 @@
-<br>
-<a class="btn btn-primary" onclick="return confirm('Are you sure? \nThis will change all of the image sizes across the website')" href="/admin/image/regenerate"> Regenerate Image Sizes </a>
--- resources/views/vendor/backpack/page/live_events.js
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from 'vue'
-import { axios } from 'axios'
-import App from './live_events.vue'
-
-const app = createApp(App)
-app.mount('#app')
-
