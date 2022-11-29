4185702aa6d78598061f16181671f9d1637c1cff
Adrian <mobistyle35@gmail.com>
2022-11-30 03:12:13 +0800

    FIX: errors

--- tests/Feature/Twitter/TwitterTest.php
+++ tests/Feature/Twitter/TwitterTest.php
@@ -2,9 +2,7 @@
 
 test('twitter has local endpoints', function () {
     $this->artisan('update:tweets');
-
     $twitterApi = $this->get('api/twitter');
-
     expect(count($twitterApi['data']))->toBe(2);
     $twitterApi->assertStatus(200);
 });
