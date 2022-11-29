d44db42bd9e6dbcf7934d633c17efdd8858d0c47
Adrian Radores <redperses143fps@gmail.com>
2022-11-29 11:38:36 +0800

    ADDED: browser testing .env

--- tests/Browser/Frontend/ExampleTest.php
+++ tests/Browser/Frontend/ExampleTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Event;
 use App\Models\Article;
+use App\Models\Day;
 use App\Models\MenuItem;
 use function DI\factory;
 use Laravel\Dusk\Browser;
@@ -11,19 +12,26 @@ use Backpack\Settings\app\Models\Setting;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
 
-// uses(DatabaseMigrations::class);
 uses(RefreshDatabase::class);
  
- 
 it('has homepage', function () {
-    MenuItem::factory()->create([
-        'name' => 'NEWS',
-        'link'  => 'NEWS'
+
+    $article = Article::factory()->create([
+        'title' => 'Adi content'
     ]);
 
-    Article::factory()->create([
-        'title' => 'Adi content'
+    $event = Event::factory()->create([
+        'title' => 'Adi event'
+    ]);
+
+    $day = Day::factory()->create([
+        'event_id' => $event->id
+    ]);
+
+    $eventReport = EventReport::factory()->create([
+        'day_id' => $day->id
     ]);
 
     $this->browse(function (Browser $browser) {
@@ -31,7 +39,12 @@ it('has homepage', function () {
     $visit->waitUntilMissingText('Loading...');
     $visit->assertSee('NEWS');
     sleep(5);
-    // $visit->assertSee('dusk event');
+    $visit->assertSee('Adi content');
+    $visit->assertSee('ADI EVENT');
+    $visit->assertSee('LIVE NOW');
     $browser->screenshot('home');
     });
+
+    \Artisan::call('migrate:fresh');
+    \Artisan::call('db:seed');
 });
